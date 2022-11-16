696cb4355b6ea8919cb75760783bb12fa899ef9e
YRGTech <117647468+YRGTech@users.noreply.github.com>
2022-11-16 08:42:22 +0100

    Add files via upload

---
+++ Fyfic final/CApp.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <SDL.h>
+#include <stdio.h>
+
+#include"CSurface.h"
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+
+class CApp {
+    private:
+        bool    Running;
+
+        //The window we'll be rendering to
+        SDL_Window* Window ;
+
+        //surface test
+        SDL_Surface* Surf_Test;
+
+    public:
+
+        CApp();
+
+        int OnExecute();
+
+        bool OnInit();
+
+        void OnEvent(SDL_Event* Event);
+
+        void OnLoop();
+
+        void OnRender();
+
+        void OnCleanup();
+
+        //The surface contained by the window
+        SDL_Surface* ScreenSurface;
+};
+
